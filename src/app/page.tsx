1c7f3bdf73d3fc413fe1485448b1e1758176c414
Simon Carr <simonjcarr@gmail.com>
2024-07-07 22:40:54 +0100

    protect images and extract to component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,21 @@
 
-import Image from "next/image";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
-import { db } from "~/server/db";
+import { Images } from "./_components/Images";
 
 export const dynamic = 'force-dynamic';
 
 export default async function HomePage() {
 
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id)
-  });
-  console.log(images)
+
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images, ...images].map((image, index) => (
-          <div key={index} className="w-48 flex flex-col">
-            <Image priority src={image.url} alt="image" className="w-full" width={200} height={200} />
-            <div>{ image.name}</div>
-          </div>
-        ))}
-      </div>
-      Hello (Gallery in progress)
+      <SignedOut>
+        <div className="h-full w-full text-2xl text-center">Please sign in above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
