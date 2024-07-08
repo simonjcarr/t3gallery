188c116b10e9aaa258a77fbc625fc62bee7a63e5
Simon Carr <simonjcarr@gmail.com>
2024-07-08 10:17:46 +0100

    scroll image names

--- src/app/_components/Images.tsx
+++ src/app/_components/Images.tsx
@@ -4,11 +4,11 @@ import { getMyImages } from "~/server/queries";
 export async function Images() {
     const images = await getMyImages();
     return (
-        <div className="flex flex-wrap gap-4">
+        <div className="flex flex-wrap gap-4 justify-center">
             {images.map((image) => (
                 <div key={image.id} className="w-48 flex flex-col">
-                    <Image priority src={image.url} alt="image" className="w-full" width={200} height={200} />
-                    <div>{image.name}</div>
+                    <Image priority src={image.url} alt="image"  height={400} width={400} style={{objectFit:'contain'}} />
+                    <div className="overflow-scroll">{image.name}</div>
                 </div>
             ))}
         </div>
